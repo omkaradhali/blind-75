d66e2ecd36730f37b253cdfba6be5e17a89c82bd
Omkar Adhali <omkar.a2989@gmail.com>
2026-02-25 17:11:35 -0500

    optimizing #4

--- blind-75/4_product_of_array_except_self.py
+++ blind-75/4_product_of_array_except_self.py
@@ -22,7 +22,7 @@ def main(arr: list) -> list:
     left = 1
     left_prod = [1]
     print(left_prod)
-    # [1,2,6,24]
+    # [1,1,2,6,24]
 
     right = 1
     right_prod = [1] 
@@ -49,6 +49,34 @@ def main(arr: list) -> list:
     return result
 
 
+# Creating just the result array and then calculating inplace.
+# Instead of creating an array of prefix and postfix calculate prefix and postfix and assign it to a variable. 
+# While calculating prefix (left to right) result will be prefix and then you update prefix for next step. 
+# while calculating postfix we need to multiply postfix with current element in arr to get product, then update the post fix by postifx * actual item in array
+def main2(arr: list) -> list:
+    result = [1] * len(arr)
+    
+    prefix = 1
+    # adding prefix product for each entry
+    # For input [1,2,3,4] the prefix array should be [1, 1, 2, 6]
+    for i in range(len(arr)):
+        # add the calculated prefix in previous step to index
+        result[i] = prefix
+        
+        # Calculate new prefix for next position
+        prefix = prefix * arr[i]
+    
+    postfix = 1
+    for i in range(len(arr) - 1, -1, -1):
+        
+        # multiply previously calculated postfix in previous step to current item in result so that you multiple prefix and postfix.
+        result[i] = postfix * result[i]
+        
+        # Calculate new postfix for next position
+        postfix = postfix * arr[i]
+    
+    print(result)
+
 if __name__ == "__main__":
-    # main([1,2,3,4])
+    # main2([1,2,3,4])
     main([-1,1,0,-3,3])
